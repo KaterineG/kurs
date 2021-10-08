6c0a8038ceeb3ecf84d9134f72206819dc299bf4
Katyur <eagimadieva@gmail.com>
2021-10-08 14:39:57 +0400

    commit

--- script/catalogFunction.js
+++ script/catalogFunction.js
@@ -1,29 +1,33 @@
-(function () {
-    let countres = [[[{ 'name': 'Бенедетто ди Биндо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Бергоньоне, Амброджо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Биссоло, Франческо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Больтраффио, Джованни', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Бонсиньори, Франческо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Боттичини, Рафаэлло', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Брамантино', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Бреа, Людовико', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Бьяджо д’Антонио Туччи', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Веккьетта', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Андреа Верроккьо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Доменико Гирландайо', 'pic': 'img/cat1.jpg', 'remark': '2 июня 1448 — 11 января 1494.', 'description': 'Один из ведущих флорентийских художников Кватроченто, основатель художественной династии, которую продолжили его брат Давид и сын Ридольфо. Глава художественной мастерской, где юный Микеланджело в течение года овладевал профессиональными навыками. Автор фресковых циклов, в которых выпукло, со всевозможными подробностями показана домашняя жизнь библейских персонажей (в их роли выступают знатные граждане Флоренции в костюмах того времени).' },
-    { 'name': 'Беноццо Гоццоли', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Граначчи, Франческо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Грегорио ди Чекко', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Джованни да Удине', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Джованни ди Паоло', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Джорджоне', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Парентино, Бернардо', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Пезеллино', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Пьетро Перуджино', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Перуцци, Бальдассаре', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Пизанелло', 'pic': '', 'remark': '', 'description': '' },
-    { 'name': 'Пинтуриккьо', 'pic': '', 'remark': '', 'description': '' },
-    ]]];
+(function() {
+    let countres = [
+        [
+            [{ 'name': 'Бенедетто ди Биндо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Бергоньоне, Амброджо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Биссоло, Франческо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Больтраффио, Джованни', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Бонсиньори, Франческо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Боттичини, Рафаэлло', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Брамантино', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Бреа, Людовико', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Бьяджо д’Антонио Туччи', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Веккьетта', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Андреа Верроккьо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Доменико Гирландайо', 'pic': 'img/cat1.jpg', 'remark': '2 июня 1448 — 11 января 1494.', 'description': 'Один из ведущих флорентийских художников Кватроченто, основатель художественной династии, которую продолжили его брат Давид и сын Ридольфо. Глава художественной мастерской, где юный Микеланджело в течение года овладевал профессиональными навыками. Автор фресковых циклов, в которых выпукло, со всевозможными подробностями показана домашняя жизнь библейских персонажей (в их роли выступают знатные граждане Флоренции в костюмах того времени).' },
+                { 'name': 'Беноццо Гоццоли', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Граначчи, Франческо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Грегорио ди Чекко', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Джованни да Удине', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Джованни ди Паоло', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Джорджоне', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Парентино, Бернардо', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Пезеллино', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Пьетро Перуджино', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Перуцци, Бальдассаре', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Пизанелло', 'pic': '', 'remark': '', 'description': '' },
+                { 'name': 'Пинтуриккьо', 'pic': '', 'remark': '', 'description': '' },
+            ]
+        ]
+    ];
 
 
 
@@ -38,12 +42,7 @@
         let link = document.getElementById('cardLink');
         if (country === 'it') {
             num = 0;
-        }
-        else if (country === 'fr') { num = 1 }
-        else if (country === 'ru') { num = 2 }
-        else if (country === 'ge') { num = 3 }
-        else if (country === 'is') { num = 4 }
-        else num = -1;
+        } else if (country === 'fr') { num = 1 } else if (country === 'ru') { num = 2 } else if (country === 'ge') { num = 3 } else if (country === 'is') { num = 4 } else num = -1;
         /*<li class="accordeon__item"> <button class="accordeon__btn">Бенедетто де
                                             Биндо</button></li> */
 
@@ -72,17 +71,16 @@
                         li.classList.add('accordeon__item');
 
                         button.textContent = countres[num][i][j].name;
-                        button.setAttribute('data-artist',i);
+                        button.setAttribute('data-artist', i);
                         parent.appendChild(li);
                         li.appendChild(button);
 
                         /*Вешаем событие по художникам */
-                        button.addEventListener('click', function (element) {
-                           
+                        button.addEventListener('click', function(element) {
+
                             if (countres[num][i][j].pic === '') {
                                 img.setAttribute('src', 'img/artists/nopic.jpg');
-                            }
-                            else {
+                            } else {
                                 img.setAttribute('src', countres[num][i][j].pic);
                             }
 
@@ -90,40 +88,38 @@
                             if (countres[num][i][j].description === '') {
                                 header.textContent = "Что мы о нем знаем?";
                                 desc.textContent = 'Пока ничего... Зато мы точно знаем, что в галерее есть на что посмотреть!';
-                                remark.style.display='none';
-                                link.style.display='block';
-                            }
-                            else {
-                                
+                                remark.style.display = 'none';
+                                link.style.display = 'block';
+                            } else {
+
                                 desc.textContent = countres[num][i][j].description;
                                 header.textContent = countres[num][i][j].name;
                                 remark.textContent = countres[num][i][j].remark;
-                                remark.style.display='block';
-                                link.style.display='none';
+                                remark.style.display = 'block';
+                                link.style.display = 'none';
                             }
 
                             if (countres[num][i][j].remark === '') {
-                               
-                            }
-                            else {
+
+                            } else {
                                 remark.textContent = countres[num][i][j].remark;
-                                
+
                             }
 
                             /*красим кнопки-художников*/
                             let buttons = document.querySelectorAll('.accordeon__btn');
                             for (let buttonItem of buttons) {
-                                buttonItem.classList.remove('accordeon__btn--focus');
+                                buttonItem.classList.remove('accordeon__btnViolet');
                             };
-                            element.currentTarget.classList.add('accordeon__btn--focus');
+                            element.currentTarget.classList.add('accordeon__btnViolet');
 
                         });
 
                         if (j === 11) {
 
-                            button.classList.add('accordeon__btn--focus');
+                            button.classList.add('accordeon__btnViolet');
                             button.click();
-                            
+
                         }
 
                     }
@@ -134,9 +130,8 @@
 
             };
             //let link = document.getElementById('cardLink').style.display = 'block';
-            
-        }
-        else {/*затираем все */
+
+        } else { /*затираем все */
             let uls = document.querySelectorAll('.accordeon__item');
 
             for (ul of uls) {
@@ -169,7 +164,7 @@
 
     /*функция заполнения пустых аккордионов */
     function emptyAccordion(parent) {
-       
+
 
         for (let l = 0; l < parent.childElementCount; l++) {
             parent.removeChild();
@@ -194,9 +189,9 @@
         title.textContent = 'Здесь пока пусто';
         parag.textContent = 'А в галерее вы всегда можете найти что-то интересное для себя';
         link.textContent = 'В галерею';
-        link.setAttribute('href','#gallery');
-        link.setAttribute('id','cardLink');
-        link.style.display='block';
+        link.setAttribute('href', '#gallery');
+        link.setAttribute('id', 'cardLink');
+        link.style.display = 'block';
         container.appendChild(left);
         container.appendChild(right);
 
@@ -206,18 +201,18 @@
         right.appendChild(link);
         parent.appendChild(container);
 
-/*событие на линк */
-link.addEventListener('click',function(el){
-    if (link.classList.contains('accordeon__blockLink--focus')) {
-        link.classList.remove('accordeon__blockLink--focus');
-    } else {
-        document.querySelectorAll('.accordeon__blockLink').forEach(function (mark) {
-            mark.classList.remove('accordeon__blockLink--focus'); //снимаем актив у всех
+        /*событие на линк */
+        link.addEventListener('click', function(el) {
+            if (link.classList.contains('accordeon__blockLink--focus')) {
+                link.classList.remove('accordeon__blockLink--focus');
+            } else {
+                document.querySelectorAll('.accordeon__blockLink').forEach(function(mark) {
+                    mark.classList.remove('accordeon__blockLink--focus'); //снимаем актив у всех
+                });
+                link.classList.add('accordeon__blockLink--focus');
+            }
+
         });
-        link.classList.add('accordeon__blockLink--focus');
-    }
-    
-});
 
     }
     /*затирка пустой карточки */
@@ -231,51 +226,48 @@ link.addEventListener('click',function(el){
         img.setAttribute('src', 'img/artists/nopic.jpg');
         header.textContent = "Что мы о нем знаем?";
         desc.textContent = 'Пока ничего... Зато мы точно знаем, что в галерее есть на что посмотреть!';
-        rem.style.display='none';
-        link.style.display='block';
+        rem.style.display = 'none';
+        link.style.display = 'block';
     }
-/*событие по художникам */
-function onAccordionListClick(element, num, i,j){
-    if (countres[num][i][j].pic === '') {
-        img.setAttribute('src', 'img/artists/nopic.jpg');
-    }
-    else {
-        img.setAttribute('src', countres[num][i][j].pic);
-    }
+    /*событие по художникам */
+    function onAccordionListClick(element, num, i, j) {
+        if (countres[num][i][j].pic === '') {
+            img.setAttribute('src', 'img/artists/nopic.jpg');
+        } else {
+            img.setAttribute('src', countres[num][i][j].pic);
+        }
 
 
-    if (countres[num][i][j].description === '') {
-        header.textContent = "Что мы о нем знаем?";
-        desc.textContent = 'Пока ничего... Зато мы точно знаем, что в галерее есть на что посмотреть!';
-        remark.style.display='none';
-        link.style.display='block';
-    }
-    else {
-        
-        desc.textContent = countres[num][i][j].description;
-        header.textContent = countres[num][i][j].name;
-        remark.textContent = countres[num][i][j].remark;
-        remark.style.display='block';
-        link.style.display='none';
-    }
+        if (countres[num][i][j].description === '') {
+            header.textContent = "Что мы о нем знаем?";
+            desc.textContent = 'Пока ничего... Зато мы точно знаем, что в галерее есть на что посмотреть!';
+            remark.style.display = 'none';
+            link.style.display = 'block';
+        } else {
 
-    if (countres[num][i][j].remark === '') {
-       
-    }
-    else {
-        remark.textContent = countres[num][i][j].remark;
-        
-    }
+            desc.textContent = countres[num][i][j].description;
+            header.textContent = countres[num][i][j].name;
+            remark.textContent = countres[num][i][j].remark;
+            remark.style.display = 'block';
+            link.style.display = 'none';
+        }
 
-    /*красим кнопки-художников*/
-    let buttons = document.querySelectorAll('.accordeon__btn');
-    for (let buttonItem of buttons) {
-        buttonItem.classList.remove('accordeon__btn--focus');
-    };
-    element.currentTarget.classList.add('accordeon__btn--focus');
-}
+        if (countres[num][i][j].remark === '') {
+
+        } else {
+            remark.textContent = countres[num][i][j].remark;
+
+        }
+
+        /*красим кнопки-художников*/
+        let buttons = document.querySelectorAll('.accordeon__btn');
+        for (let buttonItem of buttons) {
+            buttonItem.classList.remove('accordeon__btn--focus');
+        };
+        element.currentTarget.classList.add('accordeon__btn--focus');
+    }
 
 
     window.loadNames = loadNames;
-window.emptyCard = emptyCard;
+    window.emptyCard = emptyCard;
 })();
--- script/script.js
+++ script/script.js
@@ -24,68 +24,67 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 /*Выпадающий список*/
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
-    document.addEventListener('click', function (event) {
+        document.addEventListener('click', function(event) {
 
-        if (event.target.className != 'dropdown' && event.target.className != 'menu__link' && event.target.className != 'select') {
+            if (event.target.className != 'dropdown' && event.target.className != 'menu__link' && event.target.className != 'select') {
 
-            popup.forEach(function (elem) {
-                elem.classList.remove('active');
+                popup.forEach(function(elem) {
+                    elem.classList.remove('active');
 
-            })
-        }
-        /*снимем обводку у кнопок слайдера*/
-        if (event.target.className != 'gallery-svg-fill') {
-
-            popup.forEach(function (elem) {
-                elem.classList.remove('.btn_focus');
-
-            })
-        }
-    });
-
-
-    document.querySelectorAll('.select').forEach(function (selectBtn) {
-        selectBtn.addEventListener('click', function (event) {
-            const path = event.currentTarget.dataset.item;
-            const currOption = document.querySelector(`[data-dropdown="${path}"]`); //текущий выбор
-            const mark = document.querySelector(`[data-mark="${path}"]`); //текущий выбор нижнего меню
-            if (currOption.classList.contains('active')) {
-                currOption.classList.remove('active');
-            } //если установлен актив, то просто снимаем
-            else {
-                document.querySelectorAll('.option').forEach(function (dropdown) {
-                    dropdown.classList.remove('active'); //снимаем актив у всех   
-                });
-                currOption.classList.add('active'); //добавляем актив к текущему выбору
-
-
-            }
-            ;
-            /*переворачиваем стрелочки*/
-            if (mark.classList.contains('menu__icon--rotate')) {
-                mark.classList.remove('menu__icon--rotate');
-            } //если установлен menu__icon--rotate, то просто снимаем
-            else {
-                document.querySelectorAll('.menu__icon').forEach(function (marks) {
-                    marks.classList.remove('menu__icon--rotate'); //снимаем актив у всех
                 })
-
-                mark.classList.add('menu__icon--rotate'); //добавляем актив к текущему выбору
-
-
             }
-        })
+            /*снимем обводку у кнопок слайдера*/
+            if (event.target.className != 'gallery-svg-fill') {
 
+                popup.forEach(function(elem) {
+                    elem.classList.remove('.btn_focus');
+
+                })
+            }
+        });
+
+
+        document.querySelectorAll('.select').forEach(function(selectBtn) {
+            selectBtn.addEventListener('click', function(event) {
+                const path = event.currentTarget.dataset.item;
+                const currOption = document.querySelector(`[data-dropdown="${path}"]`); //текущий выбор
+                const mark = document.querySelector(`[data-mark="${path}"]`); //текущий выбор нижнего меню
+                if (currOption.classList.contains('active')) {
+                    currOption.classList.remove('active');
+                } //если установлен актив, то просто снимаем
+                else {
+                    document.querySelectorAll('.option').forEach(function(dropdown) {
+                        dropdown.classList.remove('active'); //снимаем актив у всех   
+                    });
+                    currOption.classList.add('active'); //добавляем актив к текущему выбору
+
+
+                };
+                /*переворачиваем стрелочки*/
+                if (mark.classList.contains('menu__icon--rotate')) {
+                    mark.classList.remove('menu__icon--rotate');
+                } //если установлен menu__icon--rotate, то просто снимаем
+                else {
+                    document.querySelectorAll('.menu__icon').forEach(function(marks) {
+                        marks.classList.remove('menu__icon--rotate'); //снимаем актив у всех
+                    })
+
+                    mark.classList.add('menu__icon--rotate'); //добавляем актив к текущему выбору
+
+
+                }
+            })
+
+
+        })
+        const popup = document.querySelectorAll('.dropdown');
+        //загружаем контент про италию
+        loadNames('it');
 
     })
-    const popup = document.querySelectorAll('.dropdown');
-    //загружаем контент про италию
-    loadNames('it');
-
-})
-/*option*/
+    /*option*/
 
 
 /**/
@@ -127,80 +126,87 @@ const swiperGslery = new Swiper('.gallery__swiper-container', {
 });
 
 /*состояние флага после клика */
-document.querySelectorAll('.catalog__button').forEach(function (btnFlag) {
-    btnFlag.addEventListener('click', function (elem) {
-        if (btnFlag.classList.contains('push')) {
-            btnFlag.classList.remove('push');
-        } else {
-            document.querySelectorAll('.catalog__button').forEach(function (mark) {
-                mark.classList.remove('push'); //снимаем актив у всех
-            });
-            btnFlag.classList.add('push');
-        }
-        loadNames(btnFlag.dataset.country);
+document.querySelectorAll('.catalog__button').forEach(function(btnFlag) {
+        btnFlag.addEventListener('click', function(elem) {
+                if (btnFlag.classList.contains('push')) {
+                    btnFlag.classList.remove('push');
+                } else {
+                    document.querySelectorAll('.catalog__button').forEach(function(mark) {
+                        mark.classList.remove('push'); //снимаем актив у всех
+                    });
+                    btnFlag.classList.add('push');
+                }
+                loadNames(btnFlag.dataset.country);
 
 
-    })
-    /*при клике на заголовок затираем карточку */
+            })
+            /*при клике на заголовок затираем карточку */
 
-    document.querySelectorAll('.accordion__block').forEach(function (head) {
+        document.querySelectorAll('.accordion__block').forEach(function(head) {
 
-        head.addEventListener('click', function () {
+            head.addEventListener('click', function() {
 
-            emptyCard();
+                emptyCard();
+                document.querySelectorAll('.accordion__block').forEach(function(ab) {
+                    ab.classList.remove('accordion__block-open');
+                });
+                head.classList.add('accordion__block-open');
+
+            })
         })
+
+
+
     })
+    /*accordion */
 
-
-
-})
-/*accordion */
-
-$(function () {
+$(function() {
     $("#accordion").accordion({ heightStyle: "content" });
 });
 /*поворот стрелочки аккордеона клик на хедере */
 
-document.querySelectorAll('.accordion__header').forEach(function (btnAcc) {
-    btnAcc.addEventListener('click', function (elem) {
-        const num = elem.currentTarget.dataset.num;
-        const button = document.querySelector(`[data-button="${num}"]`); //кнопка
+document.querySelectorAll('.accordion__header').forEach(function(btnAcc) {
+        btnAcc.addEventListener('click', function(elem) {
+            const num = elem.currentTarget.dataset.num;
+            const button = document.querySelector(`[data-button="${num}"]`); //кнопка
 
-        if (button.classList.contains('active')) {
-            button.classList.remove('active');
-        } //если установлен поворот, то просто снимаем
-        else {
-            document.querySelectorAll('.accordion__button').forEach(function (activBtn) {
-                activBtn.classList.remove('rotator'); //снимаем актив у всех
-            })
-            button.classList.add('rotator'); //добавляем актив к текущему выбору
+            if (button.classList.contains('active')) {
+                button.classList.remove('active');
+            } //если установлен поворот, то просто снимаем
+            else {
+                document.querySelectorAll('.accordion__button').forEach(function(activBtn) {
+                    activBtn.classList.remove('rotator'); //снимаем актив у всех
+                })
+                button.classList.add('rotator'); //добавляем актив к текущему выбору
+                //////добавляем открытый класс
+                button.classList.add('accordion__block-open');
 
 
-        }
+            }
 
 
-        /*фокусируем на первой кнопке и ставим картинку*/
-        let buttons = document.querySelectorAll('.accordeon__btn');
-        for (let buttonItem of buttons) {
-            buttonItem.classList.remove('accordeon__btn--focus');
-        };
-        let buttonFirst = document.querySelector(`[data-artist="${num-1}"]`);
-        buttonFirst.click();
+            /*фокусируем на первой кнопке и ставим картинку*/
+            let buttons = document.querySelectorAll('.accordeon__btn');
+            for (let buttonItem of buttons) {
+                buttonItem.classList.remove('accordeon__btnViolet');
+            };
+            let buttonFirst = document.querySelector(`[data-artist="${num-1}"]`);
+            buttonFirst.click();
         })
 
 
 
 
-})
-/*заголовки аккордиона */
+    })
+    /*заголовки аккордиона */
 let headers = document.querySelectorAll('.accordion__block');
 let accBtns = document.querySelectorAll('.accordion__header');
 for (let header of headers) {
-    header.addEventListener('click', function (el) {
+    header.addEventListener('click', function(el) {
         for (let accBtn of accBtns) {
-            accBtn.classList.remove('accordion__header--focus');
+            accBtn.classList.remove('accordion__headerViolet');
             if (accBtn.dataset.num === el.currentTarget.dataset.block) {
-                accBtn.classList.add('accordion__header--focus');
+                accBtn.classList.add('accordion__headerViolet');
             }
         }
 
@@ -210,12 +216,10 @@ for (let header of headers) {
     })
 }
 
-document.querySelectorAll('.gallery-svg-fill').forEach(function (button) {
-    button.addEventListener('click', function (e) {
+document.querySelectorAll('.gallery-svg-fill').forEach(function(button) {
+    button.addEventListener('click', function(e) {
         /**/
         e.currentTarget.classList.add('btn_focus');
 
     })
 });
-
-
